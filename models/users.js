3040194dfe6d04cda0880a579fc9616a32f493b0
Amit Das <dasamit8858@gmail.com>
2019-10-06 20:53:55 +0530

    partial commit

--- models/users.js
+++ models/users.js
@@ -1,8 +1,8 @@
 const mongoose= require('mongoose')
-const bcrypt = require('bcrpytjs')
+const bcrypt = require('bcryptjs')
 
 const environment = process.env.NODE_ENV;
-const stage = require('./config')[environment];
+const stage = require('../config')[environment];
 
 // schema maps to a collection
 const Schema = mongoose.Schema;
@@ -20,22 +20,21 @@ const userSchema = new Schema ({
         trim: true
     }
 })
-userSchema.pre('Save',function(next){
-    const user = this
-    if(!user.isModified && !user.isNew){
-        //do nothing since it is an old user.
-    } 
-    else {
-        bcrypt.hash(user.password,stage.saltingRounds,function(err,hash){
-            if(err){
-                console.log('Error hashing password for user', user.name);
-                next(err);
-            }
-            else {
-                user.password = hash;
-                next();
-            }
-        })
+userSchema.pre('save', function(next) {
+    const user = this;
+    if(!user.isModified || !user.isNew) { // don't rehash if it's an old user
+      next();
+    } else {
+      bcrypt.hash(user.password, stage.saltingRounds, function(err, hash) {
+        if (err) {
+          console.log('Error hashing password for user', user.name);
+          next(err);
+        } else {
+          user.password = hash;
+          next();
+        }
+      });
     }
-})
+  });
+  
 module.exports = mongoose.model('User',userSchema)
