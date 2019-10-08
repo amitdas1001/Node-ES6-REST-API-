bb8bad57b8a5a66d729a9799bccbc91a1c81ecdd
Amit Das <dasamit8858@gmail.com>
2019-10-08 21:56:26 +0530

    final commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const User = require('../models/users')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken');
 const connUri = process.env.MONGO_LOCAL_CONN_URL;
 
 module.exports = {
@@ -34,31 +34,36 @@ module.exports = {
       },
       login: (req, res) => {
         const { name, password } = req.body;
-    
+        
         mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
+          
           let result = {};
           let status = 200;
           if(!err) {
             User.findOne({name}, (err, user) => {
+              
               if (!err && user) {
-                // We could compare passwords in our model instead of below
                 
                 bcrypt.compare(password, user.password).then(match => {
-                console.log(match)
+                  
                   if (match) {
-                    status = 200
-                    //create a token
-                    const payload = {user: user.name}
-                    const options = {expiresIn: '2d', issuer: 'https://scotch.io'}
-                    const secret = process.env.JWT_SECRET
-                    const token = jwt.sign(payload,options,secret)
-                    result.token = token
+                    
+                    status = 200;
+                    // Create a token
+                    const payload = { user: user.name };
+                    const options = { expiresIn: '2d', issuer: 'https://scotch.io' };
+                    const secret = process.env.JWT_SECRET;
+                    const token = jwt.sign(payload, secret, options);
+    
+                    
+                    result.token = token;
                     result.status = status;
                     result.result = user;
+                    
                   } else {
                     status = 401;
                     result.status = status;
-                    result.error = 'Authentication error';
+                    result.error = `Authentication error`;
                   }
                   res.status(status).send(result);
                 }).catch(err => {
@@ -81,6 +86,41 @@ module.exports = {
             res.status(status).send(result);
           }
         });
+      },
+      getAll: (req, res) => {
+        mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
+          let result = {};
+          let status = 200;
+          if (!err) {
+            const payload = req.decoded;
+            // TODO: Log the payload here to verify that it's the same payload
+            //  we used when we created the token
+            // console.log('PAYLOAD', payload);
+            if (payload && payload.user === 'admin') {
+              User.find({}, (err, users) => {
+                if (!err) {
+                  result.status = status;
+                  result.error = err;
+                  result.result = users;
+                } else {
+                  status = 500;
+                  result.status = status;
+                  result.error = err;
+                }
+                res.status(status).send(result);
+              });
+            } else {
+              status = 401;
+              result.status = status;
+              result.error = `Authentication error`;
+              res.status(status).send(result);
+            }
+          } else {
+            status = 500;
+            result.status = status;
+            result.error = err;
+            res.status(status).send(result);
+          }
+        });
       }
-    
 }
